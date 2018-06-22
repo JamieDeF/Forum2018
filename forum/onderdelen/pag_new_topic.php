f27fbeb6d74caf1175e07e0139a3bbe01b79023b
Jamie de Fockert <meerca_jamie@hotmail.com>
2018-06-22 23:32:43 +0200

    ckeditor

--- forum/onderdelen/pag_new_topic.php
+++ forum/onderdelen/pag_new_topic.php
@@ -8,20 +8,16 @@ if (isset($_SESSION['user_login'])) {
     $output .="
         <form id=\"maketopic\" method=\"POST\" action=\"\">
     	    Titel:<br>
-    	    <input type=\"text\" id='select' style='width: 50%;' name=\"title\" required><br>
-    	    Content:<br>
-    	    <input type=\"text\" name=\"content\" required></textarea><br>
-    	    <input type=\"hidden\" name=\"post\" id=\"new_topic\" value=\"new_topic\">
+            <input type=\"text\" id='select' style='width: 50%;' name=\"title\" required><br>
+            Content:<br>
+            <textarea class='ckeditor' id='topic_content' name='editor'required></textarea>
+    	    <input type=\"hidden\" name=\"post\" value=\"new_topic\">
     	    <input type=\"hidden\" name=\"_token\" id=\"_token\" value=\"$token\">
             <input type=\"hidden\" name=\"thread_id\" id=\"thread_id\" value=\"$thread_id\">
-    	    <button type=\"submit\" value=\"submit\" value=\"new_topic\">Plaats</button>
+            <button type= \"submit\" onclick='CKupdate();' value=\"submit\">Submit</button>
     	</form>  
         ";
-        $output .="<script>
-  		 $(document).ready( function() {
-     	  CKEDITOR.replace('new_topic');
-   });
-</script>";
+  		  $js .=" CKEDITOR.replace('topic_content')";
 
 } else{
     // niet ingelogd? inloggen om topic aan te maken
